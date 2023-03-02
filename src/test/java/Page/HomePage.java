e128b0de5600509c1c3b5c2c585e5207f10760c3
gabriel.velez <gabriel.velez@hotmail.com>
2023-03-02 15:35:19 -0600

    Add Chrome Driver

--- src/test/java/Page/HomePage.java
+++ src/test/java/Page/HomePage.java
@@ -1,9 +1,17 @@
 package Page;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
+    protected FirefoxDriver driver;
+    public HomePage(FirefoxDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 
     //@FindBy (xpath = "user")
     @FindBy(id = "first-name")
--- src/test/java/Steps/FormSteps.java
+++ src/test/java/Steps/FormSteps.java
@@ -1,7 +1,14 @@
 package Steps;
 import Page.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class FormSteps extends HomePage{
 
+    public FormSteps(FirefoxDriver driver) {
+        super(driver);
+    }
 
     public void enterFirstName(){
         String first_name = "Gabriel";
@@ -10,4 +17,6 @@ public class FormSteps extends HomePage{
     public void Click_Highest_level_of_education(){
         this.college.click();
     }
+
+
 }
